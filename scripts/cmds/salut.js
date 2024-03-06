67fa97744f0aff1e01729893b1c8aa6fce47f4a5
Jeckazice <162384025+Jeckazice@users.noreply.github.com>
2024-03-06 01:40:11 +0100

    Update salut.js

--- scripts/cmds/salut.js
+++ scripts/cmds/salut.js
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "salut") return message.reply("𝗦𝗮𝗹𝘂𝘁 𝗯𝗿𝗼 🤵 , 𝒄𝒐𝒎𝒎𝒆𝒏𝒕 𝒕𝒖 𝒗𝒂𝒔? 𝗺𝗼𝗶 𝗰'𝗲𝘀𝘁 Tk Bot👨‍💻 𝒒𝒖'𝒆𝒔 𝒄𝒆 𝒒𝒖𝒆 𝒋𝒆 𝒑𝒆𝒖𝒙 𝒇𝒂𝒊𝒓𝒆 𝒑𝒐𝒖𝒓 𝒕𝒐𝒊 𝒕𝒂𝒑𝒆   -help pour voir ma liste 𝕕𝕖𝕤 𝕔𝕠𝕞𝕞𝕒𝕟𝕕𝕖𝕤👑🤖 ");
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("🌺𝗦𝗮𝗹𝘂𝘁, 𝒄𝒐𝒎𝒎𝒆𝒏𝒕 𝒕𝒖 𝒗𝒂𝒔? 𝗺𝗼𝗶 𝗰'𝗲𝘀𝘁 Zen.Bot 👨‍💻 𝒒𝒖'𝒆𝒔 𝒄𝒆 𝒒𝒖𝒆 𝒋𝒆 𝒑𝒆𝒖𝒙 𝒇𝒂𝒊𝒓𝒆 𝒑𝒐𝒖𝒓 𝒕𝒐𝒊 ?");
 }
 };
