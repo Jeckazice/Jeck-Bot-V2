079e27bc91ed6b779964e4a8958268d92b099741
Jeckazice <162384025+Jeckazice@users.noreply.github.com>
2024-03-06 01:37:03 +0100

    Update and rename scripts/cmds/tkgc.js to scripts/zengc.js

--- scripts/zengc.js
+++ scripts/zengc.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: {
-    name: "Tkgc",
+    name: "zengc",
     version: "1.0",
     author: "SiAM",
     countDown: 30,
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, args, message, event }) {
-    const supportGroupId = "6969548919773764"; // ID of the support group
+    const supportGroupId = "7400373346668414"; // ID of the support group
 
     const threadID = event.threadID;
     const userID = event.senderID;
@@ -30,7 +30,7 @@ module.exports = {
       if (participantIDs.includes(userID)) {
         // User is already in the support group
         api.sendMessage(
-          "✅ 𝐓𝐮 𝐚𝐬 𝐝𝐞́𝐣𝐚̀ 𝐞́𝐭𝐞́ 𝐚𝐣𝐨𝐮𝐭𝐞𝐫 𝐝𝐚𝐧𝐬 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐬𝐢 𝐭𝐮 𝐧𝐞 𝐥𝐞 𝐭𝐫𝐨𝐮𝐯𝐞 𝐩𝐚𝐬, 𝐯𝐞́𝐫𝐢𝐟𝐢𝐞 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝'𝐢𝐧𝐯𝐢𝐭𝐚𝐭𝐢𝐨𝐧 𝐩𝐚𝐬 𝐦𝐬𝐠 𝐨𝐮 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝𝐞 𝐬𝐩𝐚𝐦🔴.",
+          "✅ 𝐓𝐮 𝐚𝐬 𝐝𝐞́𝐣𝐚̀ 𝐞́𝐭𝐞́ 𝐚𝐣𝐨𝐮𝐭𝐞𝐫 𝐝𝐚𝐧𝐬 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐬𝐢 𝐭𝐮 𝐧𝐞 𝐥𝐞 𝐭𝐫𝐨𝐮𝐯𝐞 𝐩𝐚𝐬, 𝐯𝐞́𝐫𝐢𝐟𝐢𝐞 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝'𝐢𝐧𝐯𝐢𝐭𝐚𝐭𝐢𝐨𝐧 𝐩𝐚𝐬 𝐦𝐬𝐠 𝐨𝐮 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝𝐞 𝐬𝐩𝐚𝐦😉.",
           threadID
         );
       } else {
@@ -44,7 +44,7 @@ module.exports = {
             );
           } else {
             api.sendMessage(
-              "✅ 𝐓'𝐚 𝐞́𝐭𝐞́ 𝐚𝐣𝐨𝐮𝐭𝐞𝐫 𝐝𝐚𝐧𝐬 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐝𝐞 𝐦𝐨𝐧 𝐜𝐫𝐞́𝐚𝐭𝐞𝐮𝐫💌. 𝐏𝐚𝐬𝐬𝐞 𝐮𝐧 𝐛𝐧 𝐦𝐨𝐦𝐞𝐧𝐭 💬👊🤳, 𝐬𝐢 𝐭𝐮 𝐧'𝐚𝐫𝐫𝐢𝐯𝐞 𝐩𝐚𝐬 𝐚 𝐫𝐞𝐭𝐨𝐮𝐯𝐞𝐫 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐫𝐞𝐠𝐚𝐫𝐝𝐞 𝐝𝐚𝐧𝐬 𝐭𝐞𝐬 𝐢𝐧𝐯𝐢𝐭𝐚𝐭𝐢𝐨𝐧 𝐩𝐚𝐬 𝐦𝐬𝐠 𝐨𝐮 𝐝𝐚𝐧𝐬 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝𝐞 𝐬𝐩𝐚𝐦🔴.",
+              "✅ 𝐓'𝐚 𝐞́𝐭𝐞́ 𝐚𝐣𝐨𝐮𝐭𝐞𝐫 𝐝𝐚𝐧𝐬 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐝𝐞 𝐦𝐨𝐧 𝐜𝐫𝐞́𝐚𝐭𝐞𝐮𝐫💌. 𝐏𝐚𝐬𝐬𝐞 𝐮𝐧 𝐛𝐧 𝐦𝐨𝐦𝐞𝐧𝐭 💬🌺, 𝐬𝐢 𝐭𝐮 𝐧'𝐚𝐫𝐫𝐢𝐯𝐞 𝐩𝐚𝐬 𝐚 𝐫𝐞𝐭𝐨𝐮𝐯𝐞𝐫 𝐥𝐞 𝐠𝐫𝐨𝐮𝐩𝐞 𝐫𝐞𝐠𝐚𝐫𝐝𝐞 𝐝𝐚𝐧𝐬 𝐭𝐞𝐬 𝐢𝐧𝐯𝐢𝐭𝐚𝐭𝐢𝐨𝐧 𝐩𝐚𝐬 𝐦𝐬𝐠 𝐨𝐮 𝐝𝐚𝐧𝐬 𝐭'𝐚 𝐛𝐨𝐢𝐭𝐞 𝐝𝐞 𝐬𝐩𝐚𝐦😉.",
               threadID
             );
           }
